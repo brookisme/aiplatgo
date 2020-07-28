ee8434aecc3e29f67572ccfd7f8a3e34f3ee5116
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-07-28 12:23:18 -0700

    [pypi 0.0.0.3] setup: version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'aiplatgo',
   packages = ['aiplatgo'],
-  version = '0.0.0.2',
+  version = '0.0.0.3',
   description = 'CLI wrapper for `gcloud ai-platform`',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
