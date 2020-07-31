1a612b4ca2123a73fb0ad6af5ca77acca69dc81a
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-07-30 21:47:39 -0700

    include config

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from distutils.core import setup
 setup(
   name = 'aiplatgo',
   packages = ['aiplatgo'],
-  version = '0.0.0.3',
+  package_dir = {
+    'aiplatgo': 'aiplatgo'
+  }
+  version = '0.0.0.4',
   description = 'CLI wrapper for `gcloud ai-platform`',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
   url = 'https://github.com/brookisme/aiplatgo',
   download_url = 'https://github.com/brookisme/aiplatgo/tarball/0.1',
   keywords = ['python','tensorflow','gcloud','ai-platform','CLI'],
-  include_package_data=True,
+  include_package_data=False,
+  package_data={
+    'aiplatgo': ['config/*.yaml']
+  },
   data_files=[
     (
       'config',[]
